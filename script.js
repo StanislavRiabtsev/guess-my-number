a4a004b06fcaec719a636a7dd682ef76eb91c7f1
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-03 15:58:40 +0100

    Implementing Highscores

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number!🎉';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23;
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +14,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.message').textContent = 'Correct Number!🎉';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!📈';
