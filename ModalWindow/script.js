ca65d2264f167b005818e411f797883a8eda0b32
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-04 15:08:14 +0100

    Working With Classes

--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -2,10 +2,22 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close');
-
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i]);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
